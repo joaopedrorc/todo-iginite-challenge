429e0b62f49b85334f9efbcc47ff908b9983e93a
jotape02 <jpc5626@gmail.com>
2021-07-26 22:49:21 -0300

    Todo App Ignite challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const id = Math.random();
+
+    if(newTaskTitle) {
+
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      };
+
+      setTasks( state => [...state, newTask]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const checkedTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task);
+
+    setTasks(checkedTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const undeletedTask = tasks.filter(task => task.id != id);
+
+    setTasks(undeletedTask);
   }
 
   return (
